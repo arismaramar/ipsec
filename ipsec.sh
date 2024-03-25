52d4de3304ae1744a475db92afd5915b64d132db
free net <74614880+arismaramar@users.noreply.github.com>
2024-03-26 00:43:41 +0700

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Debian 9 & 10 64bit
-# Ubuntu 18.04 & 20.04 bit
-# Centos 7 & 8 64bit 
-# My Telegram : https://t.me/amantubilah
-# ==========================================
+
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -15,34 +11,9 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/arismaramar/izin/main/ip"
-checking_sc() {
-  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
-  if [[ $date_list < $useexp ]]; then
-    echo -ne
-  else
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e ""
-    echo -e "            ${RED}PERMISSION DENIED !${NC}"
-    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
-    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
-    echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      \033[0;36mTelegram${NC} t.me/amantubilah"
-    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6285365581599"
-    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    exit
-  fi
-}
-checking_sc
-clear
 # ==================================================
 # Link Hosting Kalian
-bzvpn="raw.githubusercontent.com/arismaramar/ipsec/main"
+akbarvpn="raw.githubusercontent.com/arismaramar/ssset/master/ipsec"
 
 VPN_IPSEC_PSK='myvpn'
 NET_IFACE=$(ip -o $NET_IFACE -4 route show to default | awk '{print $5}');
@@ -142,8 +113,8 @@ L2TP_LOCAL=192.168.42.1
 L2TP_POOL=192.168.42.10-192.168.42.250
 XAUTH_NET=192.168.43.0/24
 XAUTH_POOL=192.168.43.10-192.168.43.250
-DNS_SRV1=1.1.1.1
-DNS_SRV2=1.0.0.1
+DNS_SRV1=8.8.8.8
+DNS_SRV2=8.8.4.4
 DNS_SRVS="\"$DNS_SRV1 $DNS_SRV2\""
 [ -n "$VPN_DNS_SRV1" ] && [ -z "$VPN_DNS_SRV2" ] && DNS_SRVS="$DNS_SRV1"
 
@@ -273,8 +244,8 @@ refuse-chap
 refuse-mschap
 require-mschap-v2
 require-mppe-128
-ms-dns 1.1.1.1
-ms-dns 1.0.0.1
+ms-dns 8.8.8.8
+ms-dns 8.8.4.4
 proxyarp
 lock
 nobsdcomp 
@@ -316,12 +287,15 @@ mkdir -p /run/pluto
 service fail2ban restart 2>/dev/null
 service ipsec restart 2>/dev/null
 service xl2tpd restart 2>/dev/null
-wget -O /usr/bin/addl2tp https://${bzvpn}/addl2tp.sh && chmod +x /usr/bin/addl2tp
-wget -O /usr/bin/dell2tp https://${bzvpn}/dell2tp.sh && chmod +x /usr/bin/dell2tp
-wget -O /usr/bin/addpptp https://${bzvpn}/addpptp.sh && chmod +x /usr/bin/addpptp
-wget -O /usr/bin/delpptp https://${bzvpn}/delpptp.sh && chmod +x /usr/bin/delpptp
-wget -O /usr/bin/renewpptp https://${bzvpn}/renewpptp.sh && chmod +x /usr/bin/renewpptp
-wget -O /usr/bin/renewl2tp https://${bzvpn}/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
-touch /var/lib/bzstorevpn/data-user-l2tp
-touch /var/lib/bzstorevpn/data-user-pptp
+wget -O /usr/bin/addl2tp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/addl2tp.sh && chmod +x /usr/bin/addl2tp
+wget -O /usr/bin/dell2tp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/dell2tp.sh && chmod +x /usr/bin/dell2tp
+wget -O /usr/bin/addpptp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/addpptp.sh && chmod +x /usr/bin/addpptp
+wget -O /usr/bin/delpptp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/delpptp.sh && chmod +x /usr/bin/delpptp
+wget -O /usr/bin/renewpptp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/renewpptp.sh && chmod +x /usr/bin/renewpptp
+wget -O /usr/bin/renewl2tp https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/renewl2tp.sh && chmod +x /usr/bin/renewl2tp
+wget -O /var/lib/crot/data-user-l2tp "https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/l2tp-user.sh"
+wget -O /var/lib/crot/data-user-pptp "https://raw.githubusercontent.com/arismaramar/ssset/master/ipsec/pptp-user"
+touch /var/lib/crot/data-user-l2tp
+touch /var/lib/crot/data-user-pptp
 rm -f /root/ipsec.sh
+
